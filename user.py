3b494730f99efa115236e5e30e321033d95925ad
Pratham Hapani <prathamhapani.082008@gmail.com>
2026-05-14 17:56:26 +0530

    make change

--- user.py
+++ user.py
@@ -1,4 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 def Pratham(name):
     return f"Hello {name}"
 name = "Pratham"
 print(Pratham(name))
+
+SECRET = os.getenv("SECRET")
+print(SECRET)
